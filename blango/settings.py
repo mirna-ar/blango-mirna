35c13cbd690fb0a41ea3d5d0d69197857d63e4ce
Mirna Arbach <mirna2.arbach@gmail.com>
2022-06-17 20:25:29 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -33,6 +33,11 @@ class Dev(Configuration):
 
   AUTH_USER_MODEL = "blango_auth.User"
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+  ACCOUNT_ACTIVATION_DAYS = 7
+  REGISTRATION_OPEN = False
+  
   ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
   X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
   CSRF_COOKIE_SAMESITE = None
--- blango/urls.py
+++ blango/urls.py
@@ -19,6 +19,9 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 
 import blog.views
 
@@ -27,6 +30,14 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path(
+    "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",
+),
 ]
 
 if settings.DEBUG:
