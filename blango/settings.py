26c02b3ca562317a395e436fca168005981bf0b4
Mirna Arbach <mirna2.arbach@gmail.com>
2022-07-10 20:42:03 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -72,7 +72,8 @@ class Dev(Configuration):
       'allauth.account',
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
-      'rest_framework'
+      'rest_framework',
+      'rest_framework.authtoken'
   ]
 
   MIDDLEWARE = [
@@ -92,6 +93,14 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
   ]
+
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+    }
 
   ROOT_URLCONF = 'blango.urls'
 
