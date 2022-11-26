dadd2569b52e5a30e31dcad18068c7ce33a5cfb9
Mirna Arbach <mirna2.arbach@gmail.com>
2022-11-26 12:34:55 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -42,3 +42,7 @@ def post_detail(request, slug):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    print
+    return render(request, "blog/post-table.html")
